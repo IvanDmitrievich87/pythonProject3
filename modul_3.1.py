3eb77df4a36b8f8849b53589459c0ed5a8418320
Иван Дмитриевич <ivansimbirtsev36@gmail.com>
2024-09-16 20:26:45 +0300

    Задание modul_3

--- modul_3.1.py
+++ modul_3.1.py
@@ -4,11 +4,13 @@ def count_calls():
     global calls
     calls += 1
 
-def string_info(string):
+def string_info(string):                       # Принимает аргумент - строку и возвращает кортеж из:
+                                               # длины этой строки, строку в верхнем регистре, строку в нижнем регистре
     count_calls()
     return len(string), string.upper(), string.lower()
 
-def is_contains(string, list_to_search):
+def is_contains(string, list_to_search):       # Принимает два аргумента: строку и список, и возвращает True,
+                                               # если строка находится в этом списке, False - если отсутствует
     count_calls()
     string_lower = string.lower()
     return any(string_lower == item.lower() for item in list_to_search)
